64ea8b6f4e9cb8fd110fd9014d54184dcfde8b2f
submah <mahapatra.subratkumar@gmail.com>
2020-03-22 20:39:27 +0530

    Update welcome.php

--- app/welcome.php
+++ app/welcome.php
@@ -46,8 +46,8 @@ if (strlen($_SESSION['id']==0)) {
     </nav>
     <div class="container">
         <header class="jumbotron hero-spacer">
-            <h1>A Warm Welcome!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+            <h1>Congratulations!!!</h1>
+            <p>You have build this Application through docker-compose.</p>
             <p><a  href="logout.php" class="btn btn-primary btn-large">Logout </a>
             </p>
         </header>
